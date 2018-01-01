a2726a621f25dea2c2f875a706396edef18d8f0a
Luke Phelan <luke.phelan@hotmail.com>
2018-01-02 10:40:22 +1100

    Prepend northwind URL with CORS anywhere URL

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ class App extends Component {
   }
   componentDidMount() {
     console.log("Component mounted");
-    axios.get(`https://northwind.servicestack.net/customers.json`)
+    axios.get(`https://cors-anywhere.herokuapp.com/https://northwind.servicestack.net/customers.json`)
       .then(function(res) {
         customers = res.data.Customers;
         this.setState({
